1bd000c9722b6eda1a87eebea564e9768728588d
ILyazZz38 <aigozin2001@mail.ru>
2022-09-13 15:27:45 +0300

    Добавил БДшку и первые модели

---
+++ GamerNet/GamerNet/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace GamerNet.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GamerNet/GamerNet/Models/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace GamerNet.Models
+{
+    public class Game
+    {
+        String Name { get; set; }
+        int Id { get; set; }
+        //Определиться где и как хранить описание
+        int CaptureId { get; set; }
+        List<Category> Category { get; set; }
+    }
+}
