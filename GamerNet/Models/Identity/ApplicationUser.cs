3aaae361408ba70cd2a92dfd95dbf8344befee4b
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2022-10-18 23:37:03 +0300

    Добавил новый бэк

--- GamerNet/Models/Identity/ApplicationUser.cs
+++ GamerNet/Models/Identity/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace Identity_Core.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public Computer Computer { get; set; }
+        public Computer? Computer { get; set; }
+        public int? ComputerId { get; set; }
     }
 }
