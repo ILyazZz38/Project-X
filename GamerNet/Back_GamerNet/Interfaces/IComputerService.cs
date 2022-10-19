b13e2b1a8009723b3efd449191802e8234be16d5
Сергей Назаров <nazaroff.serezha2014@gmail.com>
2022-10-19 22:17:01 +0300

    Добавил интерфейсы, пофиксил аунтефикацию

---
+++ GamerNet/Back_GamerNet/Interfaces/IComputerService.cs
@@ -0,0 +1,17 @@
+﻿using Back_GamerNet.Context;
+using Models;
+
+namespace Back_GamerNet.Interfaces
+{
+    public interface IComputerService
+    {
+        public ContextApplication Context { get; set; }
+        public Computer GetComputer(string clientId);
+        public bool ChangeComputer(Computer computer);
+        /// <summary>
+        /// Метод только для админа
+        /// </summary>
+        /// <returns></returns>
+        public List<Computer> GetComputers();
+    }
+}
---
+++ GamerNet/Back_GamerNet/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using Back_GamerNet.Context;
+using Identity_Core.Models;
+using Models;
+
+namespace Back_GamerNet.Interfaces
+{
+    public interface IUserService
+    {
+        public ContextApplication Context { get; set; }
+        public ApplicationUser GetUser(string clientId, bool IsWithComputer);
+    }
+}
